27c5cc7c019a80b2153eee24dca5c591dc77efd7
kochanskicezary <c.kochanski93+1@gmail.com>
2020-10-18 13:35:03 +0200

    Add working fiding active whiprounds

--- src/main/java/pl/sda/dao/WhiproundDao.java
+++ src/main/java/pl/sda/dao/WhiproundDao.java
@@ -9,35 +9,31 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class WhiproundDao extends AbstractDao<Whipround>  {
+public class WhiproundDao extends AbstractDao<Whipround> {
 
 
     public WhiproundDao() {
         super(Whipround.class);
     }
 
-    public List<Whipround> getActiveDonation() {
-
+    public List<Whipround> getActiveWhiprounds() {
         Session session = SessionProvider.getSession();
-        List <Whipround> records = (List<Whipround>) session.createQuery("select whipround.id, whipround.goal, sum(donation.amount) " +
-                "from Whipround whipround join whipround.donation donation " +
-                "group by whipround.id")
-                .list()
-                .stream().filter(whipround->{
-                    Object [] columns = (Object[]) whipround;
-                    BigDecimal goal = new BigDecimal(columns[1].toString());
-                    BigDecimal sum = new BigDecimal(columns[2].toString());
-                    int result = goal.compareTo(sum);
-                    return result>0;
-                }).map(mapToWhipround()).collect(Collectors.toList());
-
-    };
-
-    private Function <Object, Whipround> mapToWhipround() {
-        return whipround->{
-            Object [] columns = (Object[]) whipround;
+        List<Object[]> list = session.createQuery("select whipround.id, whipround.goal, sum(donation.amount) "
+            + "from Donation donation join donation.whipround whipround "
+            + "group by whipround.id", Object[].class).list();
+        List<Whipround> records = list.stream()
+            .filter(columns -> {
+                BigDecimal goal = new BigDecimal(columns[1].toString());
+                BigDecimal sum = new BigDecimal(columns[2].toString());
+                int result = goal.compareTo(sum);
+                return result > 0;
+        })
+        .map(columns -> {
             int id = Integer.parseInt(columns[0].toString());
             return findById(id);
-        };
+        })
+        .collect(Collectors.toList());
+        session.close();
+        return records;
     }
 }
