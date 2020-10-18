d5f5221036d3c9bdc80b25f57e42298975294227
kasiakarmelita <68457701+kasiakarmelita@users.noreply.github.com>
2020-10-18 13:51:07 +0200

    Find_by_whiproud

--- src/main/java/pl/sda/dao/DonationDao.java
+++ src/main/java/pl/sda/dao/DonationDao.java
@@ -1,9 +1,23 @@
 package pl.sda.dao;
 
+
+import org.hibernate.Session;
+import pl.sda.database.SessionProvider;
 import pl.sda.model.Donation;
+
+import java.util.List;
 
 public class DonationDao extends AbstractDao<Donation> {
     public DonationDao() {
         super(Donation.class);
+    }
+
+    public List<Donation> findByWhiproundId (int id){
+        Session session = SessionProvider.getSession();
+        List<Donation> list = session.createQuery("from Donation where whipround.id =:id", Donation.class)
+                .setParameter("id", id)
+                .list();
+        session.close();
+        return list;
     }
 }
